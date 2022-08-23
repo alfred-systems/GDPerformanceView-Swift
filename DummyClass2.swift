b24a18eb99a6af8cff028aeebbb1dacc58096e5e
Steven Chen <steven.chen@alfred.camera>
2022-08-23 10:13:32 +0800

    test swift line from branch

---
+++ DummyClass2.swift
@@ -0,0 +1,13 @@
+//
+//  DummyClass2.swift
+//  GDPerformanceView-Swift
+//
+//  Created by Steven Chen on 2022/8/23.
+//  Copyright © 2022 Daniil Gavrilov. All rights reserved.
+//
+
+import Foundation
+
+class test_swift_line_branch {
+    
+}
